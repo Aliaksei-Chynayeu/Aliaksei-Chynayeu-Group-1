d855525e0065750d181a8455f19f4f73ce0d4ec7
asnodirkeai <andrei.soika@outlook.com>
2014-10-10 19:06:14 +0300

    Update run_with_specific_param.bat

--- Garabage-Collector/run_with_specific_param.bat
+++ Garabage-Collector/run_with_specific_param.bat
@@ -1,1 +1,5 @@
 java ... -XX:+UseConcMarkSweepGC -Xms=6m -Xmx=18m -XX:NewSize=2m -XX:MaxNewSize=2m -XX:PermSize=20m -XX:MaxPermSize=30m
+rem -XX:+UseConcMarkSweepGC               point out to use CMS collector
+rem -Xms=6m -Xmx=18m                      set up min/initial and max size of heap
+rem -XX:NewSize=2m -XX:MaxNewSize=2m      set up the exact size of new generation
+rem -XX:PermSize=20m -XX:MaxPermSize=30m  set up min/initial and max size of permanent generation
